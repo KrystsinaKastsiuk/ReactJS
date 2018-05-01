d608d2bf4bb2c9c388f18e552d494fd075fe837b
Krystsina Kastsiuk <kristina.evzrezova@gmail.com>
2018-05-01 18:59:43 +0300

    Add ErrorBoundary components

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import Header from './header/header.jsx';
 import Body from './body/body.jsx';
 import Footer from './footer/footer.jsx';
+import ErrorBoundary from '../errorBoundary/errorBoundary.jsx';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Body />
-        <Footer />
+        <ErrorBoundary>
+          <Header />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Body />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Footer />
+        </ErrorBoundary>
       </div>
     );
   }
