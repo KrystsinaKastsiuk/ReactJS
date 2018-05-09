7637780bf1599906181128cd6e99f9de361f1743
Krystsina Kastsiuk <kristina.evzrezova@gmail.com>
2018-05-09 23:37:57 +0300

    update cypress test

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,6 +1,6 @@
 describe('The Home Page', () => {
-  beforeEach( () => {
-    cy.exec('npm run start');
+  beforeEach(() => {
+    cy.exec('npm run start', { failOnNonZeroExit: false });
   });
 
   it('successfully loads', () => {
