02de8a4cab91924ccdbec230936247ff5d4a37f8
Krystsina Kastsiuk <kristina.evzrezova@gmail.com>
2018-05-10 01:11:52 +0300

    update functional test for errorBoundary

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+// import renderer from 'react-test-renderer';
+// import 'styled-components-test-utils';
 import App from './App';
+import './App.css';
 import Header from './header/header';
 import Body from './body/body';
 import Footer from './footer/footer';
@@ -28,4 +31,9 @@ describe('App', () => {
   it('should has a class related to footer', () => {
     expect(wrapper.find(Footer).dive().find('.footer')).toHaveLength(1);
   });
+
+  // it('should has a class', () => {
+  //   const component = renderer.create(<App />);
+  //   expect(component).toHaveStyleRule('text-align', 'center');
+  // });
 });
